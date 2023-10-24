78382a5ca1765ba6e993c8acc8d38e644268b2a2
이찬솔 <58240527+chansol790@users.noreply.github.com>
2023-10-24 11:39:45 +0900

    preprocessing

---
+++ job02_concat.py
@@ -0,0 +1,18 @@
+import datetime
+import pandas as pd
+import glob
+
+data_paths = glob.glob('./crawling_data/20*.csv')
+print(data_paths)
+
+df = pd.DataFrame()
+
+for path in data_paths:
+    df_temp = pd.read_csv(path)
+    df_temp.dropna(inplace=True)
+    df_temp.drop_duplicates(inplace=True)
+    df = pd.concat([df, df_temp], ignore_index=True)
+df.drop_duplicates(inplace=True)
+df.info()
+
+df.to_csv('./crawling_data/reviews_{}.csv'.format(datetime.datetime.now().strftime('%Y%m%d')), index=False)
