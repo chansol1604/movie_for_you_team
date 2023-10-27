e5143068c1b4d91887d4f85ffb2e9d7ea26cadba
이찬솔 <58240527+chansol790@users.noreply.github.com>
2023-10-27 11:16:17 +0900

    word2vec

--- job07_word2vec.py
+++ job07_word2vec.py
@@ -15,9 +15,9 @@ for sentence in reviews:
     tokens.append(token)
 print(tokens[0])
 
-#(tokens, 차원의 수, 끊어서 볼 숫자, , 사용할 프로세서 개수, 학습 횟수, 알고리즘)
+#(tokens, 차원의 수, 끊어서 볼 숫자, 최소 나와야 하는 크기(20번 반복해서 나온 단어만), 사용할 프로세서 개수, 학습 횟수, 알고리즘)
 # 끊어서 볼 숫자 ex> {사극, 갖추다, 뻔하다, 흐름} , {갖추다, 뻔하다, 흐름, 배우}. 데이터가 많아야 큰 값을 지정할 수 있음
-embedding_model = Word2Vec(tokens, vector_size=100, window=4, min_count=20,
+embedding_model = Word2Vec(tokens, vector_size=100, window=4, min_count=10,
                            workers=10, epochs=100, sg=1)
 embedding_model.save('./models/word2vec_movie_review.model')
 print(list(embedding_model.wv.index_to_key))
--- movie_recommendation.py
+++ movie_recommendation.py
@@ -4,6 +4,8 @@ from scipy.io import mmread
 import pickle
 from konlpy.tag import Okt
 import re
+from gensim.models import Word2Vec
+from konlpy.tag import Okt
 
 
 
@@ -22,10 +24,75 @@ Tfidf_matrix =mmread('./models/Tfidf_movie_review.mtx').tocsr()
 with open('./models/tfidf.pickle', 'rb') as f:
     Tfidf = pickle.load(f)
 
-print(df_reviews.iloc[383, 0])
-cosine_sim = linear_kernel(Tfidf_matrix[1925], Tfidf_matrix)
-print(cosine_sim[0])
-print(len(cosine_sim[0]))
+# 영화 리뷰 기반 추천
+# print(df_reviews.iloc[383, 0])
+# cosine_sim = linear_kernel(Tfidf_matrix[87], Tfidf_matrix)
+# print(cosine_sim[0])
+# print(len(cosine_sim[0]))
+# recommendation = getRecommendation(cosine_sim)
+# print(recommendation)
+
+# 키워드 기반 추천
+
+# embedding_model = Word2Vec.load('./models/word2vec_movie_review.model')
+# keyword = '마블'
+# try:
+#     sim_word = embedding_model.wv.most_similar(keyword, topn=10)
+#     print(sim_word)
+#     words = [keyword]
+#     for word, _ in sim_word:
+#         words.append(word)
+#     print(words)
+#
+#     sentence = []
+#     count = 10
+#     for word in words:
+#         sentence = sentence + [word] * count
+#         count -= 1
+#     sentence = ' '.join(sentence)
+#     print(sentence)
+#     sentence_vec = Tfidf.transform([sentence])
+#     cosine_sim = linear_kernel(sentence_vec, Tfidf_matrix)
+#     recommendation = getRecommendation(cosine_sim)
+#     print(recommendation)
+# except:
+#     print('다른 키워드를 이용하세요.')
+
+okt = Okt()
+df_stopwords = pd.read_csv('./stopwords.csv')
+stopwords = list(df_stopwords['stopword'])
+embedding_model = Word2Vec.load('./models/word2vec_movie_review.model')
+
+sentence = '화려한 액션과 소름 돋는 반전이 있는 영화'
+token_sentence = okt.pos(sentence, stem=True)
+sentences = []
+sentence_processing = []
+words = []
+
+
+for word, _ in token_sentence:
+    sentences.append(word)
+
+for sentence in sentences:
+    if len(sentence) > 1:
+        if sentence not in stopwords:
+            sentence_processing.append(sentence)
+print(sentence_processing)
+
+for keyword in sentence_processing:
+    try:
+        sim_word = embedding_model.wv.most_similar(keyword, topn=10)
+        for word, _ in sim_word:
+            words.append(keyword)
+            words.append(word)
+    except:
+        print('다른 키워드를 이용하세요.')
+sentence = []
+for word in words:
+    sentence = sentence + [word]
+sentence = ' '.join(sentence)
+print(sentence)
+sentence_vec = Tfidf.transform([sentence])
+cosine_sim = linear_kernel(sentence_vec, Tfidf_matrix)
 recommendation = getRecommendation(cosine_sim)
 print(recommendation)
-
